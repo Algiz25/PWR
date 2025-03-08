bd6402233a13cd0b7f84a86acfa93a419f962da0
Algiz25 <126596639+Algiz25@users.noreply.github.com>
2025-03-08 23:07:41 +0100

    Create DSaA 2.2.java
    List 2 task 2

---
+++ DSaA 2.2.java
@@ -0,0 +1,27 @@
+import java.util.Iterator;
+
+public class MyIterator implements Iterator<Integer> {
+    private int curr;
+
+    public MyIterator(int start) {
+        curr = start;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public Integer next() {
+        return curr++;
+    }
+
+    public static void main(String[] args) {
+        MyIterator iterator = new MyIterator(5);
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println(iterator.next());
+        }
+    }
+}
